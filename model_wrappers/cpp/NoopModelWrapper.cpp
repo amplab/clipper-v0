6a1092b7312cd7337e27aa614f7120711feefe09
Giulio Zhou <giuliozhou8@gmail.com>
2016-07-14 10:45:15 -0700

    Combine NoopModelWrapper header and cpp files into one cpp file

--- model_wrappers/cpp/NoopModelWrapper.cpp
+++ model_wrappers/cpp/NoopModelWrapper.cpp
@@ -1,6 +1,18 @@
 #include<vector>
 #include "ClipperRPC.h"
-#include "NoopModelWrapper.h"
+#include "Model.h"
+
+class NoopModelWrapper : public Model {
+    public:
+        vector<double>* predict_bytes(vector< vector<char> >& input);
+
+        vector<double>* predict_floats(vector< vector<double> >& input);
+
+        vector<double>* predict_ints(vector< vector<uint32_t> >& input);
+
+        vector<double>* predict_strings(vector< vector<string> >& input);
+};
+
 
 vector<double>* NoopModelWrapper::predict_bytes(
         vector<vector<char> >& input) {
