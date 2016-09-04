2bb80a1852c9ce5b9bf8b9fb0e76b1bbfd99b14d
Giulio Zhou <giuliozhou8@gmail.com>
2016-09-04 10:40:45 -0700

    Tensorflow Inception Model Wrapper (#47)
    Add Tensorflow Inceptionv3 model wrapper

---
+++ examples/inception-example-client.py
@@ -0,0 +1,61 @@
+from __future__ import print_function
+import sys
+import json
+import os
+import requests
+import random
+from datetime import datetime
+import skimage
+import time
+from skimage.transform import resize
+
+import numpy as np
+import skimage.io as skio
+import sklearn.linear_model
+
+DAISY_PATH = 'data/daisy'
+
+def load_inception_imgs(img_location):
+    imgs = []
+    img_files = os.listdir(img_location)
+    # Get all the jpg images (only the first 10 or so)
+    img_files = filter(lambda x: '.jpg' in x, img_files)[:10]
+    # Read in the images and reshape
+    for i in range(len(img_files)):
+        img = skio.imread(os.path.join(img_location, img_files[i]))
+        img = resize(img, (299, 299))
+        img = img.flatten().tolist()
+        imgs.append(img)
+    return imgs
+
+def inception_update(uid, x, y):
+    url = "http://localhost:1337/update"
+    req_json = json.dumps({'uid': uid, 'input': list(x), 'label': y})
+    headers = {'Content-type': 'application/json'}
+    start = datetime.now()
+    r = requests.post(url, headers=headers, data=req_json)
+    end = datetime.now()
+    latency = (end - start).total_seconds() * 1000.0
+    print("'%s', %f ms" % (r.text, latency))
+
+def inception_prediction(host, uid, x):
+    url = "http://%s:1337/predict" % host
+    req_json = json.dumps({'uid': uid, 'input': list(x)})
+    headers = {'Content-type': 'application/json'}
+    # x_str = ", ".join(["%d" % a for a in x])
+    start = datetime.now()
+    r = requests.post(url, headers=headers, data=req_json)
+    end = datetime.now()
+    latency = (end - start).total_seconds() * 1000.0
+    print("'%s', %f ms" % (r.text, latency))
+
+if __name__=='__main__':
+    args = sys.argv
+    x = load_inception_imgs(DAISY_PATH)
+    uid = 4
+    while True:
+        example_num = np.random.randint(0,len(x))
+        inception_prediction("localhost", uid, x[example_num])
+        time.sleep(1.5)
+
+
---
+++ model_wrappers/python/inception_modelwrapper.py
@@ -0,0 +1,87 @@
+from __future__ import print_function
+import numpy as np
+import skimage
+import skimage.io as skio
+import sys
+import os
+import rpc
+import tensorflow as tf
+
+from inception import inception_model as inception
+from skimage.transform import resize
+
+class InceptionModelWrapper(rpc.ModelWrapperBase):
+
+    def __init__(self, batch_size, image_size, num_classes, checkpoint_path):
+        self.batch_size = batch_size
+        self.image_size = image_size
+        self.num_classes = num_classes
+        self.checkpoint_path = checkpoint_path
+        self.reuse_scope = False
+        self.sess = tf.Session()
+        # Perform initialization of TF inception model
+        input_shape = (batch_size, image_size, image_size, 3)
+        self.jpegs = tf.placeholder(tf.float32, shape=input_shape)
+        images = tf.map_fn(self.preprocess_image, self.jpegs, dtype=tf.float32)
+        logits, _ = inception.inference(images, num_classes + 1)
+        self.top_1_op = tf.nn.top_k(logits, 1)
+        # Restore variables from training checkpoint
+        saver = tf.train.Saver()
+        saver.restore(self.sess, self.checkpoint_path)
+        # Do a sample inference to preload everything
+        self.predict_floats(np.zeros(input_shape))
+
+    def predict_ints(self, inputs):
+        inputs = map(lambda x: skimage.img_as_float(x.astype(np.ubyte)), inputs)
+        return self.predict_floats(inputs)
+
+    def predict_floats(self, inputs):
+        inputs = np.array(inputs)
+        inputs = inputs.reshape(
+            (len(inputs), self.image_size, self.image_size, 3))
+        # Pad the inputs ndarray with the first image if necessary
+        if len(inputs) < self.batch_size:
+            top_img = inputs[0].reshape((1,) + inputs[0].shape)
+            pad_imgs = np.repeat(top_img, self.batch_size - len(inputs), axis=0)
+            inputs = np.concatenate((inputs, pad_imgs), axis=0)
+        with tf.variable_scope("", reuse=self.reuse_scope) as scope:
+            top_1 = self.sess.run([self.top_1_op],
+                                  feed_dict={self.jpegs: inputs})
+        # Mark reuse_scope as True for future reuse of inception variables
+        self.reuse_scope = True
+        preds = top_1[0].indices.flatten()
+        preds = preds.astype(np.float64)
+        return preds
+
+    def preprocess_image(self, image_buffer):
+        """Preprocess JPEG encoded bytes to 3D float Tensor."""
+        # Decode the string as an RGB JPEG.
+        # Note that the resulting image contains an unknown height and width
+        # that is set dynamically by decode_jpeg. In other words, the height
+        # and width of image is unknown at compile-time.
+        image = tf.image.convert_image_dtype(image_buffer, dtype=tf.float32)
+        # After this point, all image pixels reside in [0,1)
+        # until the very end, when they're rescaled to (-1, 1).  The various
+        # adjust_* ops all require this range for dtype float.
+        image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+        # Crop the central region of the image with an area containing 87.5% of
+        # the original image.
+        image = tf.image.central_crop(image, central_fraction=0.875)
+        # Resize the image to the original height and width.
+        image = tf.expand_dims(image, 0)
+        image = tf.image.resize_bilinear(image,
+                                         [self.image_size, self.image_size],
+                                         align_corners=False)
+        image = tf.squeeze(image, [0])
+        # Finally, rescale to [-1,1] instead of [0, 1)
+        image = tf.sub(image, 0.5)
+        image = tf.mul(image, 2.0)
+        return image
+
+
+
+if __name__=='__main__':
+    model_path = os.environ["CLIPPER_MODEL_PATH"]
+    print(model_path, file=sys.stderr)
+    model = InceptionModelWrapper(1, 299, 1000, model_path)
+    rpc.start(model, 6001)
