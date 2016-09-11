c2f9782be5e6e6834f7de43d4af762f3123c5151
Dan Crankshaw <dscrankshaw@gmail.com>
2016-09-11 14:51:39 -0700

    reran TF Mnist comparison with faster rpc

--- model_wrappers/python/run_tf_conv_mw.sh
+++ model_wrappers/python/run_tf_conv_mw.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 CLIPPER_MODEL_PATH=/crankshaw-local/tf_models/mnist_from_guilio/mnist_convnet/tf_checkpoint/convnet/convnet.ckpt \
-  TF_CONV_BATCH_SIZE=150 \
+  TF_CONV_BATCH_SIZE=512 \
   python tf_convnet_mnist_modelwrapper.py
