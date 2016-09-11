f40e9308c27b72227e9c233e51caf7d5d0cf7bc1
Giulio Zhou <giuliozhou8@gmail.com>
2016-09-11 09:33:23 -0700

    Fixes to Cifar10 model wrapper to support large batches

--- model_wrappers/python/cifar10_tf_modelwrapper.py
+++ model_wrappers/python/cifar10_tf_modelwrapper.py
@@ -29,29 +29,38 @@ class Cifar10TfModelWrapper(rpc.ModelWrapperBase):
         saver = tf.train.Saver()
         saver.restore(self.sess, self.checkpoint_path)
         # Do a sample inference to preload everything
-        # self.predict_floats(np.zeros(input_shape))
+        self.predict_floats(np.zeros(input_shape))
 
     def predict_ints(self, inputs):
         inputs = map(lambda x: skimage.img_as_float(x.astype(np.ubyte)), inputs)
         return self.predict_floats(inputs)
 
     def predict_floats(self, inputs):
+        num_inputs = len(inputs)
+        num_batches = num_inputs / self.batch_size
         inputs = np.array(inputs)
-        inputs = inputs.reshape(
-            (len(inputs), self.image_size, self.image_size))
-        # Pad the inputs ndarray with the first image if necessary
-        if len(inputs) < self.batch_size:
-            top_img = inputs[0].reshape((1,) + inputs[0].shape)
-            pad_imgs = np.repeat(top_img, self.batch_size - len(inputs), axis=0)
-            inputs = np.concatenate((inputs, pad_imgs), axis=0)
-        with tf.variable_scope("", reuse=self.reuse_scope) as scope:
-            top_1 = self.sess.run([self.top_1_op],
-                                  feed_dict={self.jpegs: inputs,
-                                             self.keep: self.dropout})
+        padded_inputs = inputs.reshape((len(inputs), self.image_size, self.image_size, 3))
+        # find how much padding we need
+        if num_batches * self.batch_size < num_inputs:
+            padding = self.batch_size - num_inputs % self.batch_size
+            top_img = padded_inputs[0].reshape((1,) + padded_inputs[0].shape)
+            pad_imgs = np.repeat(top_img, padding, axis=0)
+            padded_inputs = np.concatenate((padded_inputs, pad_imgs), axis=0)
+            assert len(padded_inputs) / self.batch_size == num_batches + 1
+
+        assert len(padded_inputs) % self.batch_size == 0
+        num_batches = len(padded_inputs) / self.batch_size
+        preds = []
+        for b in range(num_batches):
+            with tf.variable_scope("", reuse=self.reuse_scope) as scope:
+                top_1 = self.sess.run([self.top_1_op],
+                                      feed_dict={self.jpegs: padded_inputs[b*self.batch_size: (b+1)*self.batch_size]})
+                cur_preds = top_1[0].indices.flatten()
+                preds.extend(cur_preds.astype(np.float64))
         # Mark reuse_scope as True for future reuse of inception variables
         self.reuse_scope = True
-        preds = top_1[0].indices.flatten()
-        preds = preds.astype(np.float64)
+        preds = np.array(preds[:num_inputs])
+        print("Predicted: %s" % preds)
         return preds
 
 
