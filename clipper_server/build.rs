4aefae4decf056fedb79a4d8f7734824289cc698
Dan Crankshaw <dscrankshaw@gmail.com>
2016-10-05 09:52:01 -0700

    updates dependency versions

--- clipper_server/build.rs
+++ clipper_server/build.rs
@@ -1,18 +1,4 @@
-// extern crate serde_codegen;
-//
-// use std::env;
-// use std::path::Path;
-//
-// pub fn main() {
-//     let out_dir = env::var_os("OUT_DIR").unwrap();
-//
-//     let src = Path::new("src/main.rs.in");
-//     let dst = Path::new(&out_dir).join("main.rs");
-//
-//     serde_codegen::expand(&src, &dst).unwrap();
-// }
-
-#[cfg(not(feature = "serde_macros"))]
+#[cfg(not(feature = "serde_derive"))]
 mod inner {
     extern crate serde_codegen;
 
@@ -35,7 +21,7 @@ mod inner {
     }
 }
 
-#[cfg(feature = "serde_macros")]
+#[cfg(feature = "serde_derive")]
 mod inner {
     pub fn main() {}
 }
